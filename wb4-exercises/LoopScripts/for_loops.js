1cbf64221bcca3383c3204935f44144afe28c431
Bryan <bmiller@tb.yearup.org>
2022-10-11 15:34:55 -0400

    did exercise 1-2 slide 23 page 2-9. for_loops.js and while_loops.js

---
+++ wb4-exercises/LoopScripts/for_loops.js
@@ -0,0 +1,6 @@
+//EXERCISE 1 / SLIDE-23/ PAGE 2-9
+phrase1 = "I love loops"
+
+for(let i=0; i<7; i++){
+    console.log(phrase1);    
+}
---
+++ wb4-exercises/LoopScripts/while_loops.js
@@ -0,0 +1,7 @@
+//EXERCISE 2 / SLIDE-23 / PAGE 2-9
+let i = 0;
+
+while(i<7){
+    console.log("I Love Loops!");
+    i++
+}
