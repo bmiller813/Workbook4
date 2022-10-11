6a1ecd106695bfb4937c6b1bd44eacfc83d6b48f
Bryan <bmiller@tb.yearup.org>
2022-10-11 13:15:20 -0400

    exercise 1 slide 13 label_maker.js finished

--- wb4-exercises/ObjectScripts/label_maker.js
+++ wb4-exercises/ObjectScripts/label_maker.js
@@ -1,5 +1,10 @@
-let contactInfo = {name: name, address: address, city: city, state: state, zip: zip};
+//EXERCISE 1 / SLIDE-13 / PAGE 1-9 = Print Address Object AKA label_maker.js
+function printContact(contactInfo){
+    console.log(contactInfo.name);
+    console.log(contactInfo.address);
+    console.log(contactInfo.city + ",", contactInfo.state, contactInfo.zip);
+}
+//TEST
+let myInfo = { name: "Pursalane Faye", address: "121 Main Street", city: "Atlanta", state: "GA", zip: "30083" };
 
-//printContact(contactInfo){
-  //  let myInfo = {name: "Pursalane Faye", address: ""};
-//}
+printContact(myInfo);
