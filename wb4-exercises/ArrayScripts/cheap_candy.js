9a91909c7adec515e3a8c557f9d37e81dad25718
Bryan <bmiller@tb.yearup.org>
2022-10-13 18:57:02 -0400

    Working on cheap_candy.js

--- wb4-exercises/ArrayScripts/cheap_candy.js
+++ wb4-exercises/ArrayScripts/cheap_candy.js
@@ -1,1 +1,25 @@
 //EXERCISE 2 / SLIDE-27 / PAGE 2-23
+
+let products = [
+    {product: "Gummy Worms", price: 5.79},
+    {product: "Plain M&Ms", price: 2.89},
+    {product: "Peanut M&Ms", price: 2.89},
+    {product: "Swedish Fish", price: 3.79},
+
+    //TODO: fill the array with 10 candles of various price ranges
+];
+
+function getCheap(prod){
+    for(let r = 0; r<products.length; r++){
+        let p = products.price
+        if(p <= 4){
+            console.log("UNDER $4: " + prod);
+        }else {
+            console.log("struggle");
+        }
+    }
+}
+
+for(let i = 0; i< products.length; i++){
+    getCheap(products[i]);
+}
